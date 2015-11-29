327867dbf3e7414f3430dc00dc448d6e68164ddc
Corey Edwards <corey@cedwardsmedia.com>
2015-11-29 11:04:21 -0600

    Added repo name and commit icon

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+
+$repo = trim(substr(getcwd(), strripos(getcwd(), "/"), strlen(getcwd())), "/");
+
 $output = array();
 exec("git log",$output);
 $history = array();
@@ -34,11 +38,11 @@ foreach($output as $line){
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> <!-- FontAwesome -->
 	<script src="js/modernizr.js"></script> <!-- Modernizr -->
 
-	<title>Responsive Vertical Timeline</title>
+	<title>Git Log: <?php echo $repo; ?></title>
 </head>
 <body>
 	<header>
-		<h1>Responsive Vertical Timeline</h1>
+		<h1><?php echo $repo; ?></h1>
 	</header>
 
 	<section id="cd-timeline" class="cd-container">
@@ -48,12 +52,13 @@ foreach($history as $item) {
 ?>
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-picture">
-				<img src="img/cd-icon-picture.svg" alt="Picture">
+				<i class="fa fa-clock-o fa-3x"></i>
 			</div> <!-- cd-timeline-img -->
 
 			<div class="cd-timeline-content">
 				<h2><?php echo "Commit: " . substr($item['hash'], 0, 8);?></h2>
-				<p><?php echo $item['message'];?></p>
+				<p class="author">Committed by: <?php echo $item['author'];?></p>
+				<p class="message"><?php echo $item['message'];?></p>
 				<a href="https://www.github.com/cedwardsmedia/git-timeline/commit/<?php echo trim($item['hash'], " "); ?>" class="cd-read-more">Read more</a>
 				<span class="cd-date"><?php echo $item['date'];?></span>
 			</div> <!-- cd-timeline-content -->
